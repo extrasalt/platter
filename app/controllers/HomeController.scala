7870f1bc9b0051c59055abb1811511cd863f3494
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-07-19 22:30:09 +0530

    Add mongodb with dummy insertion and Add list to template

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,11 +1,16 @@
 package controllers
 
+import java.util.concurrent.TimeUnit
 import javax.inject._
 
+import com.mongodb.async.client.Observer
+import models.Tweet
+import org.mongodb.scala._
 import play.api._
 import play.api.mvc._
-import views.html.helper.CSRF
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
@@ -21,10 +26,15 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    val tweets = List("Hello", "It's me", "How are you?", "Hello from the other side")
+    Ok(views.html.index(tweets))
   }
 
   def submit() = Action{ implicit request: Request[AnyContent] =>
+
+    val collection = MongoClient().getDatabase("platter").getCollection("tweet")  
+    Await.result(collection.insertOne(Document("content"->"it's me")).toFuture, Duration(10, TimeUnit.SECONDS))
+
     Redirect(routes.HomeController.index()).flashing("success"-> "tweet saved")
   }
 
--- build.sbt
+++ build.sbt
@@ -6,9 +6,19 @@ version := "1.0-SNAPSHOT"
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.12.2"
-
 libraryDependencies += guice
+libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "2.1.0"
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.0" % Test
+
+
+//
+//libraryDependencies += "com.mongodb.casbah" %% "casbah" % "2.1.5-1"
+//libraryDependencies += "com.novus" %% "salat-core" % "0.0.8-20120223"
+//libraryDependencies += "commons-codec" % "commons-codec" % "1.6"
+
+// https://mvnrepository.com/artifact/org.mongodb.scala/mongo-scala-driver_2.11
+
+
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "org.extrasalt.controllers._"
