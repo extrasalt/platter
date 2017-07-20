b07203633f13af92cb9b6220b83531d13fed7335
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-07-20 12:22:22 +0530

    Extract model from controller code

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,37 +4,26 @@ import javax.inject._
 
 import org.mongodb.scala._
 import play.api.mvc._
-
+import models._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents)
     extends AbstractController(cc) {
 
+  val mongoClient = MongoClient()
+
   def index() = Action.async { implicit request: Request[AnyContent] =>
-    val ft = MongoClient()
-      .getDatabase("platter")
-      .getCollection("tweet")
-      .find()
-      .toFuture()
-
-    ft.map({ s =>
-        s.map((x: Document) => x.getString("content"))
-      })
-      .map(contentList => Ok(views.html.index(contentList.toList)))
-
+    Tweet.all.map(contentList => Ok(views.html.index(contentList.toList)))
   }
 
   def submit() = Action.async { implicit request: Request[AnyContent] =>
     val content = request.body.asFormUrlEncoded.get("content").head
 
-    val collection = MongoClient().getDatabase("platter").getCollection("tweet")
-    val future = collection.insertOne(Document("content" -> content)).toFuture
-
-    future.map(
-      s =>
-        Redirect(routes.HomeController.index())
-          .flashing("success" -> "tweet saved"))
+    Tweet(content).save
+      .map(
+        s => Redirect(routes.HomeController.index)
+      )
   }
 
 }
--- app/models/Tweet.scala
+++ app/models/Tweet.scala
@@ -1,8 +1,33 @@
 package models
 
-/**
-  * Created by mohan on 19/7/17.
-  */
-class Tweet {
+import org.mongodb.scala.{Completed, MongoClient, MongoCollection}
+import org.mongodb.scala.bson.collection.immutable.Document
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+case class Tweet(content: String) {
+
+  def document: Document = {
+    Document("content" -> content)
+  }
+
+  def save: Future[Completed] = {
+    Tweet.tweetCollection.insertOne(this.document).toFuture
+  }
+
+}
+
+object Tweet {
+  val tweetCollection: MongoCollection[Document] =
+    MongoClient().getDatabase("platter").getCollection("tweet")
+
+  def all: Future[Seq[String]] = {
+    tweetCollection
+      .find()
+      .toFuture()
+      .map((s: Seq[Document]) =>
+        s.map((doc: Document) => doc.getString("content")))
+  }
 
 }
